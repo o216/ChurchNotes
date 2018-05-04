158de4a9e8fc16d0e2ff23e1bf31ceba62c47abf
Osana Osaretin <osana.osaretin@gmail.com>
2018-05-03 23:27:25 -0400

    Keep the creds to myself

--- db.js
+++ db.js
@@ -1,14 +1,4 @@
-const firebase = require('firebase');
-require("firebase/firestore");
-
-firebase.initializeApp({
-  apiKey: "AIzaSyBH71f8hjyDq4O8m5tqsn5GxTiWLfLMy_g",
-  authDomain: "reflect-4c46e.firebaseapp.com",
-  databaseURL: "https://reflect-4c46e.firebaseio.com",
-  projectId: "reflect-4c46e",
-  storageBucket: "",
-  messagingSenderId: "600197178871"
-});
+const firebase = require('./creds.js');
 
 const firestore = firebase.firestore();
 const settings = {timestampsInSnapshots: true};
